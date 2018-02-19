bf03bdeca81596cef5aa1241375be8625db401a3
Quasimodo <quasimodo1660@gmail.com>
2018-02-18 16:57:28 -0800

    build routes and change the server config file

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
+import { AlphaComponent } from './alpha/alpha.component';
+import { BetaComponent } from './beta/beta.component';
+import { GammaComponent } from './gamma/gamma.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'alpha',component: AlphaComponent },
+  { path: 'beta',component: BetaComponent },
+  // use a colon and parameter name to include a parameter in the url
+  { path: 'gamma/:id', component: GammaComponent },
+  { path: 'gamma', component: GammaComponent },
+  { path: '', pathMatch: 'full', redirectTo: '/alpha' },
+  { path: '**', component: PagenotfoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { AlphaComponent } from './alpha/alpha.component';
+import { BetaComponent } from './beta/beta.component';
+import { GammaComponent } from './gamma/gamma.component';
+import { PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AlphaComponent,
+    BetaComponent,
+    GammaComponent,
+    PagenotfoundComponent
   ],
   imports: [
     BrowserModule,
